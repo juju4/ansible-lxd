1efa29db3c12a9890113a972ebd0dd1452c7ce79
juju4 <juju4@users.noreply.github.com>
2016-08-13 18:20:06 -0400

    sync repositories

--- test/integration/default/serverspec/lxd_spec.rb
+++ test/integration/default/serverspec/lxd_spec.rb
@@ -7,7 +7,7 @@ describe service('lxd') do
   it { should be_enabled   }
   it { should be_running   }
 end
-describe service('lxd-bridge') do  
+describe service('lxd-bridge'), :if => os[:family] == 'ubuntu' && os[:release] == '16.04' do
   it { should be_enabled   }
   it { should be_running   }
 end
