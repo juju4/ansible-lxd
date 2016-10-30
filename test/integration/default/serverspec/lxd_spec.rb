e5ed4d96aa0fdc3a6073aec5499a6c21c8e09976
juju4 <juju4@users.noreply.github.com>
2016-10-29 20:52:49 -0400

    sync repositories - galaxy meta fix, serverspec review, cleaning

--- test/integration/default/serverspec/lxd_spec.rb
+++ test/integration/default/serverspec/lxd_spec.rb
@@ -3,12 +3,21 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
+describe package('lxd') do
+  it { should be_installed }
+end
+
+describe file('/usr/bin/lxd') do
+  it { should be_file }
+  it { should be_executable }
+end
+
 ## FIXME! running test fails on travis/xenial: https://travis-ci.org/juju4/ansible-lxd/jobs/152119834
-describe service('lxd') do  
+describe service('lxd'), :if => os[:family] == 'ubuntu' && os[:release] == '14.04' do
   it { should be_enabled   }
   it { should be_running   }
 end
-describe service('lxd-bridge'), :if => os[:family] == 'ubuntu' && os[:release] == '16.04' do
+describe service('lxd-bridge'), :if => os[:family] == 'ubuntu' && os[:release] != '14.04' do
   it { should be_enabled   }
   it { should be_running   }
 end
