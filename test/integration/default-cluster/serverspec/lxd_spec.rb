292805bebd0b4a79ac61880097bf31aeddbaba84
juju4 <juju4@users.noreply.github.com>
2019-06-29 15:12:37 -0400

    make separate test suite default-cluster

---
+++ test/integration/default-cluster/serverspec/lxd_spec.rb
@@ -0,0 +1,43 @@
+require 'serverspec'
+
+# Required by serverspec
+set :backend, :exec
+
+describe package('lxd') do
+  it { should be_installed }
+end
+
+describe file('/usr/bin/lxd') do
+  it { should be_file }
+  it { should be_executable }
+end
+
+describe service('lxd'), :if => os[:family] == 'ubuntu' do
+  it { should be_enabled }
+  it { should be_running }
+end
+describe service('lxd-bridge'), :if => os[:family] == 'ubuntu' && os[:release] == '16.04' do
+  it { should be_enabled }
+#  it { should be_running }
+end
+
+describe file('/var/lib/lxd/unix.socket') do
+  it { should be_socket }
+end
+
+describe interface('lxdfan0') do
+  it { should exist }
+end
+
+describe interface('lxdbr0') do
+  it { should exist }
+end
+
+describe port(8443) do
+  it { should be_listening.with('tcp') }
+end
+
+# FIXME: there is no ipv6 on travis
+# describe port(8443) do
+#   it { should be_listening.with('tcp6') }
+# end
