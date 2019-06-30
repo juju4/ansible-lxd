a6b17e3f24aab8e66dc3fa9724b1c73eb15bde8a
juju4 <juju4@users.noreply.github.com>
2019-06-29 22:12:53 -0400

    test/default: remove lxd-bridge test - not on lxd3

--- test/integration/default/serverspec/lxd_spec.rb
+++ test/integration/default/serverspec/lxd_spec.rb
@@ -16,10 +16,6 @@ describe service('lxd'), :if => os[:family] == 'ubuntu' do
   it { should be_enabled }
   it { should be_running }
 end
-describe service('lxd-bridge'), :if => os[:family] == 'ubuntu' && os[:release] == '16.04' do
-  it { should be_enabled }
-#  it { should be_running }
-end
 
 describe file('/var/lib/lxd/unix.socket') do
   it { should be_socket }
