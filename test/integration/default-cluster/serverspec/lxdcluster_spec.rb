3230b2d57eb050f467c59ee9fba34aace6e38bab
juju4 <juju4@users.noreply.github.com>
2019-06-29 21:19:18 -0400

    test/default-cluster: update lxc cluster list check

--- test/integration/default-cluster/serverspec/lxdcluster_spec.rb
+++ test/integration/default-cluster/serverspec/lxdcluster_spec.rb
@@ -11,7 +11,7 @@ end
 
 describe command('lxc cluster list') do
   its(:stdout) { should match /ONLINE/ }
-  its(:stdout) { should match /bin/ }
-  its(:stderr) { should match /No such file or directory/ }
+  its(:stdout) { should match /fully operational/ }
+  its(:stderr) { should match /^$/ }
   its(:exit_status) { should eq 0 }
 end
