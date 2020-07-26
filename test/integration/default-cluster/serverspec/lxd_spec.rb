e6e76a10b1077d7f241be0a089ff0934e4d3a7e1
juju4 <juju4@users.noreply.github.com>
2020-07-26 12:52:04 -0400

    travis, test/default* serverspec: review as 20.04 use snap lxd (4)

--- test/integration/default-cluster/serverspec/lxd_spec.rb
+++ test/integration/default-cluster/serverspec/lxd_spec.rb
@@ -3,12 +3,6 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
-if (os[:family] == 'ubuntu' && os[:release] == '20.04')
-  set snap_lxd = true
-else
-  set snap_lxd = false
-end
-
 describe package('lxd') do
   it { should be_installed }
 end
@@ -18,7 +12,7 @@ describe file('/usr/bin/lxd') do
   it { should be_executable }
 end
 
-describe service('lxd'), :if => os[:family] == 'ubuntu' and not snap_lxd do
+describe service('lxd'), :if => os[:family] == 'ubuntu' && !(os[:family] == 'ubuntu' && os[:release] == '20.04') do
   it { should be_enabled }
   it { should be_running }
 end
@@ -27,11 +21,11 @@ describe service('lxd-bridge'), :if => os[:family] == 'ubuntu' && os[:release] =
 #  it { should be_running }
 end
 
-describe file('/var/lib/lxd/unix.socket'), :if => os[:family] == 'ubuntu' and not snap_lxd do
+describe file('/var/lib/lxd/unix.socket'), :if => os[:family] == 'ubuntu' && !(os[:family] == 'ubuntu' && os[:release] == '20.04') do
   it { should be_socket }
 end
 
-describe command('snap info'), :if => os[:family] == 'ubuntu' and snap_lxd do
+describe command('snap list'), :if => (os[:family] == 'ubuntu' && os[:release] == '20.04') do
   its(:stdout) { should match /lxd/ }
   its(:stderr) { should match /^$/ }
   its(:exit_status) { should eq 0 }
